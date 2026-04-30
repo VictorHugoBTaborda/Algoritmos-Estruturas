eaa81f53382a44a041214ffd89f4b69325044ab7
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-04-30 07:59:49 -0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//                    0    1  2   3   4
+const precosLivros = [25, 15, 30, 50, 45, 20];
 
+let maisBarato = 0;
+
+for (let atual = 0; atual < precosLivros.length; atual++) {
+      if(precosLivros[atual] < precosLivros[maisBarato])
+maisBarato = atual
+
+}
