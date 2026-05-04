a619cfcf387aceace8c17e89ba3c3f44ef685d84
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-05-04 11:55:06 -0300

    menorValor.js

--- menorValor.js
+++ menorValor.js
@@ -3,14 +3,13 @@ const Livros = require('./listaLivros');
 function menorValor(arrProdutos, posicaoInicial){
       let maisBarato = 0;
 
-for (let atual = 0; atual < livros.length; atual++) {
-      if(livros[atual].preco < livros[maisBarato]){
+for (let atual = 0; atual < arrProdutos.length; atual++) {
+      if(arrProdutos[atual].preco < arrProdutos[maisBarato]){
       maisBarato = atual
 
       }
 }
-
+return maisBarato;
 }
 
-
-console.log('O livro mais barato custa ${livros[maisBarato].preco} e o título é ${livro[mais].titulo}')
+module.exports = menorValor;
