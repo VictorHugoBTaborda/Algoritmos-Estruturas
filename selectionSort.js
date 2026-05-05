4a962752c4195da04a9f9f2394ff2ba04d144a75
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-05-05 08:02:19 -0300

    selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -2,6 +2,11 @@ const livros = require('./listaLivros');
 const menorValor = require('./menorValor');
 
 for (let atual = 0; atual < livros.length; atual++) {
+ let menor = menorValor(livros, atual)   
 
-    
+let livroAtual = livros[atual];
+let livroMenorValor = livro[menor];
+
+livros[atual] = livroMenorPreco
+livro[menor] = livroAtual
 }
