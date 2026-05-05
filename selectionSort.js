7a111fcf4e392b1f882789c89f33b306d8f44222
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-05-05 08:41:38 -0300

    selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -8,7 +8,7 @@ for (let atual = 0; atual < livros.length; atual++) {
 let livroAtual = livros[atual];
 console.log('livro atual', livros[atual])
 let livroMenorpreco = livros[menor];
-console.log('livro menor preço', livros[atual])
+console.log('livro menor preço', livros[menor])
 
 livros[atual] = livroMenorPreco
 livro[menor] = livroAtual
